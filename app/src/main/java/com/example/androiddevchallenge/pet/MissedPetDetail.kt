366e8acd49cdaaafd633fd5dc3f1670f266dd82e
leeyookyoung <metoo303@gmail.com>
2021-03-02 04:06:40 +0900

    version 4

--- app/src/main/java/com/example/androiddevchallenge/pet/MissedPetDetail.kt
+++ app/src/main/java/com/example/androiddevchallenge/pet/MissedPetDetail.kt
@@ -2,14 +2,39 @@ package com.example.androiddevchallenge.pet
 
 
 import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.*
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.FractionalThreshold
+import androidx.compose.material.Surface
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Female
 import androidx.compose.material.icons.filled.Male
 import androidx.compose.material.icons.rounded.ArrowBack
-import androidx.compose.runtime.*
+import androidx.compose.material.rememberSwipeableState
+import androidx.compose.material.swipeable
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.IconButton
+import androidx.compose.material.Icon
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.material.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Divider
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.material.LocalContentAlpha
+import androidx.compose.material.ContentAlpha
+
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
